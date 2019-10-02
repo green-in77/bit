b116f860a1d4910ffd9786c3f8607575cb5e8485
Green <green.in77@gmail.com>
2019-10-02 16:59:45 +0900

    Oracle JDBC 191002

---
+++ Database/JDBC/Labs/JDBC/src/Ex02_Oracle_DML.java
@@ -0,0 +1,70 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Ex02_Oracle_DML {
+	public static void main(String[] args) {
+		Connection conn = null;
+		Statement stmt = null;
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver"); //메모리에 로드. >> new Driver
+			System.out.println("오라클 드라이버 로딩");
+			
+			//1. DB연결
+			conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:xe","bituser","1004");
+			System.out.println("연결끊어졌니?? " + conn.isClosed() + " (False >> 연결)"); //너 연결 끊어졌니?? -> 아니!!! false
+			
+			//2. 명령생성(CRUD)
+			stmt = conn.createStatement();
+			
+			//3. 실행구문
+			//String sql = "insert into dememp(empno,ename,deptno) values (100,'홍길동',10)";
+			
+			//입력받는 값을 insert
+			int empno=0;
+			String ename="";
+			int deptno=0;
+			
+			Scanner sc = new Scanner(System.in);
+			
+			System.out.println("사번 입력");
+			empno = Integer.parseInt(sc.nextLine());
+			
+			System.out.println("이름 입력");
+			ename = sc.nextLine();
+			
+			System.out.println("부서번호 입력");
+			deptno = Integer.parseInt(sc.nextLine());
+			
+			//고전적인방법
+			String sql = "insert into dememp(empno,ename,deptno) values ("+empno+",'"+ename+"',"+deptno+")";
+			
+			//현대적인방법(parameter 처리 효율적으로..) >> 실제로 사용하는 것
+			//String sql = "insert into dememp(empno,ename,deptno) values (?,?,?)";
+			//String sql = "insert into dememp(empno,ename,deptno) values (100,'홍길동',10)";
+			
+			//4. 실행 (결과집합X, 반영된 행의 갯수 return)
+			int resultrow = stmt.executeUpdate(sql);
+			
+			//5. 처리
+			if(resultrow > 0) {
+				System.out.println("반영된 행의 갯수 : " + resultrow);
+			}else {
+				//의미가 없음. 반영된 행이 없다면 // insert 가 안됐다면... 에러!!!!!catch 예외로 빠짐...
+				System.out.println("Insert Fail : " + resultrow);
+			}		
+		}catch(Exception e) {
+			System.out.println("Insert Fail 예외" + e.getMessage());
+		}finally {
+			try {
+				stmt.close();
+				conn.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ Database/JDBC/Labs/JDBC/src/Ex09_Oracle_Preparestatement_dml3.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import kr.or.bit.utils.ConnectionHelper;
+import kr.or.bit.utils.SingletonHelper;
+
+public class Ex09_Oracle_Preparestatement_dml3 {
+	public static void main(String[] args) {
+		//delete from dememp where deptno=deptno;
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		
+		try {
+			conn=SingletonHelper.getConnection("oracle");
+			String sql = "delete from dememp where deptno=?";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, 100);
+			
+			int row = pstmt.executeUpdate(); //insert, update, delete 모두 실행문은 같음
+			
+			if(row>0) {
+				System.out.println("delete row count : " + row);
+			}else {
+				System.out.println("delete fail");
+			}
+			
+		}catch(Exception e){
+			System.out.println("delete fail" + e.getMessage());
+		}finally {
+			ConnectionHelper.close(pstmt);
+		}
+
+	}
+
+}
