5531ab62ef7e86b638caf6f688345f8c01548e8e
Green <green.in77@gmail.com>
2019-12-03 18:08:08 +0900

    191203 Spring Basic - DI개념

---
+++ SpringLabs/SpringDI_Basic01/src/DI2/NewRecord.java
@@ -0,0 +1,52 @@
+package DI2;
+
+//VO 또는 DTO, Domain 의미를 갖는 클래스(데이터를 담을 수 있는 클래스)
+public class NewRecord {
+	private int kor;
+	private int eng;
+	private int math;
+	
+	public NewRecord() {}
+
+	public NewRecord(int kor, int eng, int math) {
+		super();
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+	}
+	
+//필요시 : 별도의 함수 추가 가능-----------------------------------------------------------------------
+	public int total() {
+		return this.kor + this.eng + this.math;
+	}
+	
+	public float avg() {
+		return total()/3.0f;
+	}
+//필요시 : 별도의 함수 추가 가능-----------------------------------------------------------------------	
+	
+	public int getKor() {
+		return kor;
+	}
+
+	public void setKor(int kor) {
+		this.kor = kor;
+	}
+
+	public int getEng() {
+		return eng;
+	}
+
+	public void setEng(int eng) {
+		this.eng = eng;
+	}
+
+	public int getMath() {
+		return math;
+	}
+
+	public void setMath(int math) {
+		this.math = math;
+	};
+	
+}
---
+++ SpringLabs/SpringDI_Basic01/src/DI3/NewRecordView.java
@@ -0,0 +1,45 @@
+package DI3;
+
+import java.util.Scanner;
+
+public class NewRecordView implements RecordView {
+	//점수 출력하는 클래스
+	private NewRecord record;
+	
+	//1. [생성자]를 통해서 필요한 객체를 생성하거나 주입 >> DI_1
+//	public NewRecordView(int kor, int eng, int math) {
+//		record = new NewRecord(kor, eng, math);
+//	}
+	
+	//2. 함수([setter])를 통해서 필요한 객체를 주입 >> DI_2
+//	public void setRecord(NewRecord record) {
+//		//함수의 parameter로 객체의 주소를 받는다.
+//		this.record = record;
+//	}
+	
+	//3. 인터페이스 활용
+	public void setRecord(Record record) {//다형성 record 를 구현한 모든 객체의 주소값이 올 수 있음
+		//함수의 parameter를 인터페이스로 
+		this.record = (NewRecord) record;
+	}
+	
+	@Override
+	public void print() {
+		System.out.println(record.total() + " / " + record.avg());
+	}
+
+	@Override
+	public void input() {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("kor : ");
+		record.setKor(sc.nextInt());
+		
+		System.out.println("eng : ");
+		record.setEng(sc.nextInt());
+		
+		System.out.println("math : ");
+		record.setMath(sc.nextInt());
+	}
+
+}
